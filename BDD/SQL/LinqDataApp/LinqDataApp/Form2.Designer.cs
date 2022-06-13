63def137155a3f7352d6be0616ccfafbfe4ce365
unknown <djamelhemch.pro@gmail.com>
2022-06-13 15:15:09 +0100

    started C# CRUD application solution

---
+++ BDD/SQL/LinqDataApp/LinqDataApp/Form2.Designer.cs
@@ -0,0 +1,70 @@
+﻿
+namespace LinqDataApp
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tablesCombo = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tablesCombo
+            // 
+            this.tablesCombo.FormattingEnabled = true;
+            this.tablesCombo.Location = new System.Drawing.Point(286, 71);
+            this.tablesCombo.Name = "tablesCombo";
+            this.tablesCombo.Size = new System.Drawing.Size(121, 21);
+            this.tablesCombo.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(200, 149);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(312, 145);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tablesCombo);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox tablesCombo;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ BDD/SQL/LinqDataApp/LinqDataApp/Form2.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinqDataApp
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            getData();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void getData()
+        {
+
+            string connetionString;
+
+            SqlConnection conx;
+            connetionString = @"Data Source=DESKTOP-LE69RUT\CESIEATS;Initial Catalog=CESI-EATS;User ID=admin;Password=arsenik98";
+            conx = new SqlConnection(connetionString);
+            conx.Open();
+            SqlCommand command;
+            SqlDataReader dataReader;
+            String sql, Output = "";
+
+            sql = "SELECT * FROM INFORMATION_SCHEMA.TABLES where TABLE_TYPE='BASE TABLE'";
+
+            command = new SqlCommand(sql, conx);
+            
+            dataReader = command.ExecuteReader();
+            while (dataReader.Read())
+            {
+                tablesCombo.Items.Add((string)dataReader["TABLE_NAME"]);                           
+            }
+
+            if (tablesCombo.SelectedItem !=null)
+            {
+                String selected = this.tablesCombo.GetItemText(this.tablesCombo.SelectedItem);
+                MessageBox.Show(selected);
+                sql = "SELECT * FROM '" + selected + "'";
+                command = new SqlCommand(sql, conx);
+                dataReader = command.ExecuteReader();
+                
+                while (dataReader.Read())
+                {
+                 Output = Output + "UserID: " + dataReader.GetValue(0) + "\n" + "User Name: " + dataReader.GetValue(1) + "\n" + "User Mail: " + dataReader.GetValue(2) + "\n";
+                }
+
+                label1.Text = selected + " Table Informations : "+"\n\n"+ Output;
+            }
+
+            dataReader.Close();
+            command.Dispose();
+        }
+
+       
+    }
+}
+    
