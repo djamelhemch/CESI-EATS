ecc35826faf3ab09e17a13ab153b83cf2db781a2
Amodias <saidamokranebelgacem@gmail.com>
2022-06-13 12:30:52 +0000

    added POST DELETE GET API

--- BDD/MongoDB/routes/menu.js
+++ BDD/MongoDB/routes/menu.js
@@ -22,5 +22,26 @@ router.post('/post', function(req, res, next) {
 
 });
 
+//get by id from collection
+router.get('/get/:id', function(req, res, next) {
+    var id= req.params.id;
+    menuModel.findById(id)
+     .then(menuModel => res.status(200).json(menuModel))
+     .catch(error => res.status(400).json({ error }));
+});
 
+// get all from collection
+router.get('/getall', function(req, res, next) {
+    menuModel.find()
+     .then(menuModel => res.status(200).json(menuModel))
+     .catch(error => res.status(400).json({ error }));
+});
+
+// delete by id from collection
+router.delete('/delete/:id', function(req, res, next) {
+    var id= req.params.id;
+    menuModel.find({id : id}).remove()
+     .then(menuModel => res.status(200).json(menuModel))
+     .catch(error => res.status(400).json({ error }));
+});
 module.exports = router;
